c1b4385f63f76a919cacc3cdb86781e1205ed1d9
Louis Chan <louischan@oursky.com>
2023-09-26 15:29:06 +0800

    Use dynamic import to work around "window is not defined" error

--- src/context/ClientAuthContext.tsx
+++ src/context/ClientAuthContext.tsx
@@ -3,38 +3,6 @@
 import React, { createContext, useCallback, useContext, useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 
-import authgear, { PromptOption } from '@authgear/web';
-
-const authgearInit = async () => {
-  try {
-    await authgear.configure({
-      endpoint: `${process.env.NEXT_PUBLIC_AUTHGEAR_ENDPOINT}`,
-      clientID: `${process.env.NEXT_PUBLIC_CLIENT_ID}`,
-      sessionType: 'refresh_token',
-    });
-
-    // console.log({
-    //   endpoint: `${process.env.NEXT_PUBLIC_AUTHGEAR_ENDPOINT}`,
-    //   clientID: `${process.env.NEXT_PUBLIC_CLIENT_ID}`,
-    //   sessionType: 'refresh_token',
-    // });
-    
-  } catch (e) {
-    console.error(e);
-  }
-};
-
-const authgearStartAuthentication = async () => {
-  try {
-    await authgear.startAuthentication({
-      redirectURI: `${process.env.NEXT_PUBLIC_BASE_URL}/auth-redirect/`,
-      prompt: PromptOption.Login,
-    });
-  } catch (e) {
-    console.error(e);
-  }
-};
-
 interface ClientAuthContextValue {
   isLoggedIn: boolean;
   setIsLoggedIn: (val: boolean) => void;
@@ -51,13 +19,45 @@ function ClientAuthContextProviderInner({ children }: { children: React.ReactNod
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [showLoginModal, setShowLoginModal] = useState<boolean>(false);
 
-  const startAuth = useCallback(() => {
-    authgearStartAuthentication()
+  const authgearInit = useCallback(async () => {
+    try {
+      const authgear = (await import("@authgear/web")).default;
+
+      await authgear.configure({
+        endpoint: `${process.env.NEXT_PUBLIC_AUTHGEAR_ENDPOINT}`,
+        clientID: `${process.env.NEXT_PUBLIC_CLIENT_ID}`,
+        sessionType: 'refresh_token',
+      });
+
+      // console.log({
+      //   endpoint: `${process.env.NEXT_PUBLIC_AUTHGEAR_ENDPOINT}`,
+      //   clientID: `${process.env.NEXT_PUBLIC_CLIENT_ID}`,
+      //   sessionType: 'refresh_token',
+      // });
+    } catch (e) {
+      console.error(e);
+    }
+  }, []);
+
+  const startAuth = useCallback(async () => {
+    try {
+      const {
+        default: authgear,
+        PromptOption,
+      }= await import("@authgear/web");
+
+      await authgear.startAuthentication({
+        redirectURI: `${process.env.NEXT_PUBLIC_BASE_URL}/auth-redirect/`,
+        prompt: PromptOption.Login,
+      });
+    } catch (e) {
+      console.error(e);
+    }
   }, [])
 
   useEffect(() => {
     authgearInit();
-  }, []);
+  }, [authgearInit]);
 
   const contextData: ClientAuthContextValue = {
     isLoggedIn,
